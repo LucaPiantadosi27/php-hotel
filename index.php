fee15ba1048a5bdfdd8c16ea03f1c273b2b7c425
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-18 17:22:13 +0100

    parking rate

--- index.php
+++ index.php
@@ -12,6 +12,59 @@ Bonus:
 NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
 Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel. (modificato)  -->
 
+<?php
+
+$parking = $_GET['parking'];
+
+
+
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -22,7 +75,33 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
     <title>PHP- Hotels</title>
 </head>
 <body>
-    
+<div class="container">
+        <table class="table bg-primary ">
+
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Vote</th>
+                    <th>Distance to Center</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                    foreach($hotels as $selHotel) {
+                        echo "<tr>";
+                        foreach($selHotel as $value) {
+                            echo "<td>$value</td>";
+                        }
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+
+        </table>
+    </div>
 
 
 <!-- Bootstrap -->
