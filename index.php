9b85cfd6acd883652528324c0759f63003ee890c
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-18 15:50:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- Descrizione
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo, da bravi sviluppatori, aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus:
+1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+3 - Fare tutto in un'unica pagina: nella stessa pagina verrà mostrato l'elenco di tutti gli hotel e in base alla compilazione del form verrà mostrato al di sotto (dopo il refresh della pagina) l'elenco di hotel filtrato correttamente
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel. (modificato)  -->
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP- Hotels</title>
+</head>
+<body>
+    
+
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
